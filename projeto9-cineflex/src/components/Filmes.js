bd16d9cf14c81d7045c54893c76294466a0b17e8
Gustavo Hideki <gustavohideki@outlook.com>
2022-05-22 23:08:27 -0300

    Finalizado layout componentizado da primeira página

--- projeto9-cineflex/src/components/Filmes.js
+++ projeto9-cineflex/src/components/Filmes.js
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, useParams, Link } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components";
 
 export default function Filmes() {
 
     const [items, setItems] = useState([]);
-    const [dados, setDados] = useState([]);
 
     useEffect(() => {
         const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
@@ -26,11 +26,11 @@ export default function Filmes() {
         <div className="home">
             <div className="barra-topo">CINEFLEX</div>
             <div className="titulo-secao">Selecione o filme</div>
-            <div className="filmes">
+            <ContainerFilmes>
                 {items.length === 0 ? "": items.map((filme) =>(
                     <Filme foto={filme.posterURL} id={filme.id} alt="" />
                 ))}
-            </div>
+            </ContainerFilmes>
         </div>
     );
 }
@@ -40,11 +40,30 @@ function Filme({ foto, id }) {
     return (
         <>
         <Link to={`/sessoes/${id}`}>
-            <div className="filme">
-                <img src={foto} />
-            </div>
+            <Foto src={foto} />
         </Link>
         </>
     );
 }
 
+const ContainerFilmes = styled.div`
+    width: 100%;
+    height: auto;
+
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+`;
+
+const Foto = styled.img`
+    width: 41%;
+    height: fit-content;
+
+    border: 8px solid #FFFFFF;
+    border-radius: 3px;
+
+    margin: 8px;
+
+    box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+`;
